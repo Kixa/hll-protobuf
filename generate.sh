23311e4927869c263faee007861be7180c61f12b
Michael Coward <mikey@kixa.me>
2022-02-10 14:13:50 +0000

    Enable the use of this as a separate go module and modify generated cc/h proto (generate.sh) to be excluded from go builds to eliminate redundant cgo requirement

--- generate.sh
+++ generate.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-protoc --cpp_out=./cpp/ \
+protoc --cpp_out=. \
        --go_opt=paths=source_relative \
-       --go_out=./go \
+       --go_out=. \
        hll.proto
+
+# Add exclusions to the c++ so we don't need cgo.
+echo -e -n "// go:build exclude\n// +build exclude\n\n" | cat - hll.pb.cc > hll.pb.cc.tmp
+mv hll.pb.cc.tmp hll.pb.cc
+
+echo -e -n "// go:build exclude\n// +build exclude\n\n" | cat - hll.pb.h > hll.pb.h.tmp
+mv hll.pb.h.tmp hll.pb.h
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/kixa/hll-protobuf
+
+go 1.17
+
+require google.golang.org/protobuf v1.27.1
+
+retract v1.0.1 // requires cgo
